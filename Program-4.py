d6dc2866559a7ddc7ae039dd592de8ac23775dfb
Tejasrihub <116421566+Tejasrihub@users.noreply.github.com>
2022-10-22 12:47:44 +0530

    Update Program-4.py

--- Program-4.py
+++ Program-4.py
@@ -1,1 +1,15 @@
-
+# python program to count the frequency of elements in a list using a dictionary.
+def CountFrequency (my_list);
+# Creating an empty dictionary
+freq = {}
+for item in my_list:
+  if (item in freq):
+    freq[item] +=1
+    else:
+      freq[item] =1
+      for key , value in freq.item():
+        print ("% d :  % d" %(key, value))
+        # Driver function 
+        if__name__=="__main__":
+          my_list = [1,2,3,4,5,6,7,8,9]
+            countFrequency(my_list)
